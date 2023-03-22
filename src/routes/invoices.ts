93ce9b6bf57bb08c6c213316a031b0d7b63b9665
Roberto Sousa <robertosousa186@gmail.com>
2023-03-21 23:26:48 -0300

    Creating new route

--- src/routes/invoices.ts
+++ src/routes/invoices.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { HighestInvoiceController } from '../controllers/HighestInvoiceController';
 import { LowestInvoiceController } from '../controllers/LowestInvoiceController';
 
 const lowestInvoice = new LowestInvoiceController();
+const highestInvoice = new HighestInvoiceController();
 
 const router = Router();
 
 router.get('/menorvalor', lowestInvoice.handle);
+router.get('/maiorvalor', highestInvoice.handle);
 
 export default router;
