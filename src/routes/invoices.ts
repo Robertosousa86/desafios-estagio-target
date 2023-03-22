ca4bed2d8a68c294060cdecc3ba334f6ebdaef4f
Roberto Sousa <robertosousa186@gmail.com>
2023-03-22 00:56:40 -0300

    Creating new route

--- src/routes/invoices.ts
+++ src/routes/invoices.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import { AboveAverageController } from '../controllers/AboveAverageController';
 import { HighestInvoiceController } from '../controllers/HighestInvoiceController';
 import { LowestInvoiceController } from '../controllers/LowestInvoiceController';
 
 const lowestInvoice = new LowestInvoiceController();
 const highestInvoice = new HighestInvoiceController();
+const aboveAverage = new AboveAverageController();
 
 const router = Router();
 
 router.get('/menorvalor', lowestInvoice.handle);
 router.get('/maiorvalor', highestInvoice.handle);
+router.get('/acimadamediamensal', aboveAverage.handle);
 
 export default router;
