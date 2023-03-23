a86fc9b7a046f5b2e40b165457294e42d5b28804
Roberto Sousa <robertosousa186@gmail.com>
2023-03-22 21:17:22 -0300

    Deleting files

--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import percentageOfRepresentation from './percentageOfRepresentation';
-
-const router = express.Router();
-
-router.use('/', percentageOfRepresentation);
-
-export default router;
