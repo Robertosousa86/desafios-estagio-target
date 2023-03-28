7b55b0c43ad3cab6cf85a60e2f40b4e038906b2e
Roberto Sousa <robertosousa186@gmail.com>
2023-03-28 20:00:09 -0300

    Updating app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-// import router from './routes';
+import router from './routes';
 
 const app = express();
 
 app.use(express.json());
-// app.use(router);
+app.use(router);
 
 export default app;
